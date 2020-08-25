b0d282c1e127a0dd236752d80bed651eb21a5ea8
wahyuandhi <wakay.98@gmail.com>
2020-08-25 11:42:06 +0700

    UPDATE
    Penambahan fitur history

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -65,9 +65,19 @@ function performCalculation() {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
+    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+
     calculator.operator = null;
     calculator.waitingForSecondNumber = false;
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
 
 const buttons = document.querySelectorAll(".button");
